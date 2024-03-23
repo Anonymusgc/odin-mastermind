abb9ce475986aa6d0b3b4b8a69b118829a72813e
Anonymusgc <120783414+Anonymusgc@users.noreply.github.com>
2024-03-23 17:51:51 +0100

    Add human player class

--- script.rb
+++ script.rb
@@ -8,18 +8,34 @@ module Mastermind
   class Game
     def initialize
       @turns = 8
+      @game_end = false
+      @codemaker = ComputerPlayer.new
+      @codebreaker = HumanPlayer.new
     end
 
-    def play; end
+    def play
+      self.secret_code = codemaker.create_code
+      until game_end
 
-    attr_accessor :turns
+      end
+    end
+
+    attr_accessor :turns, :game_end, :secret_code, :codemaker, :codebreaker
   end
 
-  class Player
+  # human player class
+  class HumanPlayer
+    def initialize
+      @type = 'human'
+    end
   end
 
   # computer player class
   class ComputerPlayer
+    def initialize
+      @type = 'computer'
+    end
+
     def create_code
       code = Array.new(HOLES)
       code.map do
